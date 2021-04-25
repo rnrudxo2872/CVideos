5d3a7a7012e48019bd4bcd45549f9f5f7a8e14dd
unknown <rnrudxo2872@naver.com>
2021-04-26 00:22:36 +0900

    footer and form scss update

--- middlewares.js
+++ middlewares.js
@@ -1,5 +1,5 @@
-import routes from "./routes"
 import multer from "multer"
+import routes from "./routes"
 
 const multerVideo = multer({dest:'uploads/videos/'});
 
@@ -7,7 +7,7 @@ export const localsMiddleWare = (req,res,next) =>{
     res.locals.siteName = "CTube";
     res.locals.routes = routes;
     res.locals.user = {
-        isAuthenticated : true,
+        isAuthenticated : false,
         id:1
     }
     next();
